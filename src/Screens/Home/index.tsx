a3c35b7775fc3beb67374de66ed241d1f14bbd66
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-09-28 20:45:07 -0600

    installing toast message to tell the user if the Country ISO is empty or if something went wrong

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
 import {trackPromise} from 'react-promise-tracker';
+import {showMessage} from 'react-native-flash-message';
 
 import axios from '../../lib/axiosClient';
 
@@ -10,13 +11,38 @@ export const Home = () => {
   const [bankISO, setBankISO] = useState('');
   const [bankList, setBankList] = useState([]);
 
+  const isInvalidISO = () => {
+    if (bankISO.length < 2) {
+      showMessage({
+        message: 'Country ISO must be 2 characters.',
+        type: 'danger',
+        autoHide: true,
+      });
+      return true;
+    }
+    return false;
+  };
+
   const searchBanks = async () => {
-    const {
-      data: {data},
-    } = await trackPromise(
-      axios.get(`/banks_by_country?country_code=${bankISO}`),
-    );
-    setBankList(data);
+    if (isInvalidISO()) {
+      return;
+    }
+
+    try {
+      const {
+        data: {data},
+      } = await trackPromise(
+        axios.get(`/banks_by_country?country_code=${bankISO}`),
+      );
+      setBankList(data);
+    } catch (error) {
+      console.log(error);
+      showMessage({
+        message: 'Something went wrong. Try again with diferent Country ISO',
+        type: 'danger',
+        autoHide: true,
+      });
+    }
   };
 
   return (
